771afade76c5c7a0eb4cf160e4769a4c63abafc8
lyliu <lyliu@thoughtworks.com>
2018-09-10 23:00:15 +0800

    [Liangyun LIU] refactor : add columns in book list information

--- src/com/twu/biblioteca/service/BookService.java
+++ src/com/twu/biblioteca/service/BookService.java
@@ -8,6 +8,7 @@ public class BookService {
 
     public void listAllBooks(ArrayList<Book> bookList) {
         System.out.print("图书列表: \n");
+        System.out.print("书名     作者名     出版年\n");
         for (int i = 0; i < bookList.size(); i++) {
             Book book = bookList.get(i);
             System.out.println(book.getBookName() + ", "
